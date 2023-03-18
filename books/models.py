8bebe194b3b9e7197752b1ff79dd3bb0489f2c7c
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-18 14:59:52 +0100

    Added book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,13 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    choice_of_cover = (
+        ("H", "Hard"),
+        ("S", "Soft"),
+    )
+    title = models.CharField(max_length=20)
+    author = models.CharField(max_length=30)
+    cover = models.CharField(max_length=1, choices=choice_of_cover)
+    inventory = models.IntegerField(validators=[MinValueValidator(1)])
