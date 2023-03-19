fe75bcbb8c651d5371d46b91a8eb57cc13aba63e
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-19 17:19:04 +0100

    Started borrowings app

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .views import RegisterUserView, CreateUserTokenView, UserDetailView
+from .views import RegisterUserView, UserDetailView
 
 
 
