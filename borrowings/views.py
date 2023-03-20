2ec2a988ba21fd99e1ff906aee8a88e50a24052b
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-20 19:29:22 +0100

    Added all non-admins can see only their borrowings & borrowings are available only for authenticated users

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-from books.models import Book
 from .models import Borrowing
 from .serializers import BorrowingSerializer, BorrowingCreateSerializer
 
@@ -8,13 +8,23 @@ from .serializers import BorrowingSerializer, BorrowingCreateSerializer
 class BorrowingList(generics.ListCreateAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         if self.request.method == 'POST':
             return BorrowingCreateSerializer
         return BorrowingSerializer
 
+    def get_queryset(self):
+        """
+        This view should return a list of all the borrowings
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Borrowing.objects.filter(client=user)
+
 
 class BorrowingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
