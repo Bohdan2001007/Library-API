8645783bd5d72f8da8817bd4b9552f197d4fdac4
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-23 18:59:03 +0100

    Fixed bag

--- Library_API/settings.py
+++ Library_API/settings.py
@@ -165,7 +165,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=120),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1200),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKEN": True,
 }
